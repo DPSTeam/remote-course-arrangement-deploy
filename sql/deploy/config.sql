bb42f88c8efa7be8fe21a82d94dd88b4ee71680b
DGideas <dgideas@outlook.com>
2016-02-07 16:19:56 +0800

    sync

--- sql/deploy/config.sql
+++ sql/deploy/config.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `config` (
-	`config_id` int(11) NOT NULL,
+	`config_id` int(11) NOT NULL auto_increment,
 	`config_name` text NOT NULL,
 	`config_value` text,
 	PRIMARY KEY (`config_id`)
 );
 ALTER TABLE `config`  DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
