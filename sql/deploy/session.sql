c1ac88f2733c84d0f90c942ef997aaedb59cf0d8
DGideas <dgideas@outlook.com>
2016-02-08 20:52:01 +0800

    minor changes

--- sql/deploy/session.sql
+++ sql/deploy/session.sql
@@ -1,9 +1,12 @@
 CREATE TABLE IF NOT EXISTS `edu_session` (
 	`session_id` int(11) NOT NULL AUTO_INCREMENT,
 	`session_time` text NOT NULL,
-	`session_key` text NOT NULL,
+	`session_token` text NOT NULL,
 	`session_ip` text NOT NULL,
 	`session_status` text NOT NULL,
 	`session_user` text DEFAULT NULL,
 	PRIMARY KEY (`session_id`)
 ) AUTO_INCREMENT=1 ;
+ALTER TABLE `edu_session` ADD UNIQUE (
+ `session_id` 
+);
